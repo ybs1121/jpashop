a3062419e15f28e2def4430f2c2046073e61bcd2
yangbyoungseok <ybs991@naver.com>
2022-06-22 17:32:14 +0900

    프로젝트 환경설정

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Setter
+public class Hello {
+    private String data;
+//
+//    public static void main(String[] args) {
+//        Hello hello = new Hello();
+//        hello.setData("123");
+//
+//        System.out.println(hello.getData());
+//    }
+
+}
---
+++ src/test/java/jpabook/jpashop/Member/MemberRepositoryTest.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.Member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+//    @Rollback(value = false)
+    void testMember() throws Exception{
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        assertThat(member.getId()).isEqualTo(findMember.getId());
+        assertThat(member.getUsername()).isEqualTo(findMember.getUsername());
+        assertThat(member).isEqualTo(findMember);
+
+    }
+
+
+
+
+}
