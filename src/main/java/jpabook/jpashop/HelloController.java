afcb43d17ec4bd9a6c65c25584fb82ff7d53b007
yangbyoungseok <ybs991@naver.com>
2022-09-13 21:21:43 +0900

    회원등록 API

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -1,16 +1,20 @@
 package jpabook.jpashop;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @GetMapping("hello")
-    public String hello(Model model){
-        model.addAttribute("data","hello!!");
-        return "hello";
+    @GetMapping("/test")
+    public ResponseEntity<String> hello(){
+        String testData = "test";
+        return ResponseEntity.status(HttpStatus.OK).body(testData);
     }
 }
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +18,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotBlank
     private String name;
 
     @Embedded
