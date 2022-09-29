096d2f4c80231ad2cfcc9b39286d460c14fd3eb5
yangbyoungseok <ybs991@naver.com>
2022-09-29 14:32:32 +0900

    주문 조회 V6

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,7 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
@@ -81,6 +82,13 @@ public class OrderApiController {
     @GetMapping("/api/v5/orders")
     public List<OrderQueryDto> orderV5(){
         return orderQueryRepository.findAllDto_optimization();
+    }
+
+    @GetMapping("/api/v6/orders")
+    public List<OrderFlatDto> orderV6(){
+        List<OrderFlatDto> flats = orderQueryRepository.findAllDto_flat();
+
+        return flats;
     }
 
 
